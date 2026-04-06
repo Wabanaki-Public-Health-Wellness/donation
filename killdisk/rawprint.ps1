3a04afd0735c93307a8e199a00943630c91d4ad0
d-wphw <dhaughton@wabanakiphw.org>
2026-04-06 14:45:02 -0400

    comments and cleanup

--- killdisk/rawprint.ps1
+++ killdisk/rawprint.ps1
@@ -2,13 +2,12 @@
 # Revision: 3/30/26
 # Description: Script to utilize the RAW printing port of a printer.
 
-
-
-
 $printerIP = "192.168.131.8"
 $port = 9100
 $inputFolder = "D:\certificates"
 $archiveFolder = "D:\archive"
+
+# Opens and closes a connection to the printer.
 function SendPDF {
     param(
         $printerIP,
@@ -29,38 +28,18 @@ function SendPDF {
     $client.Close()
 }
 
+# Creates archive folder if not detected.
 if(!(Test-Path $archiveFolder)) {
     New-Item -ItemType Directory -Path $archiveFolder | Out-Null
 }
 
+# Loops to check if any new sanit. certs are in the certs folder, sends it to the printer and then moves it to the archive folder.
 while ($true) {
-    if (Test-NetConnection $printerIP -Port $port){ 
+    #if (Test-NetConnection $printerIP -Port $port){ 
         Get-ChildItem $inputFolder -Filter *.pdf | ForEach-Object {
             SendPDF -printerIP $printerIP -file $_.FullName
             Move-Item $_.Fullname $archiveFolder -Force
         }
-    }
+    #}
     Start-Sleep -Seconds 5
 }
-
-# Get-ChildItem $inputFolder -Filter *.pdf | ForEach-Object {
-#     $file = $_.FullName
-#     $destination = Join-Path $archiveFolder $_.Name
-
-#     try {
-#         Write-Host "Printing $($_.Name)"
-
-#         SendPDF -printerIP $printerIP -file $file
-#         Start-Sleep -Seconds 2
-
-#         Move-Item $file $destination -Force
-
-#         Write-Host "Moved to archive"
-#     }
-#     catch{
-#         Write-Host "Failed to print $($_.Name)"
-#         Write-Host $_
-#     }
-# }
-
-
